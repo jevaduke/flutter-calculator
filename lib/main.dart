62253134b2933d5d74c1d1c2be0c0a09c5b330a6
jevaduke <jevaduke@gmail.com>
2022-05-22 22:23:00 +0530

    update1

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:calculator/calu_ui.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Calculator',
+        theme: ThemeData(
+          primarySwatch: createMaterialColor(Color(0xFF5582AC)),
+        ),
+        home: CalcUI());
+  }
+
+  MaterialColor createMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    Map<int, Color> swatch = {};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    for (var strength in strengths) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    }
+    ;
+    return MaterialColor(color.value, swatch);
+  }
+}
